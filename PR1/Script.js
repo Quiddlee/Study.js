fecfee4c5018ec77ed38f3133edaa60237e5f956
Bogdan <thegarrybit@gmail.com>
2022-09-23 18:20:03 +0300

    Ver1 msivi

---
+++ PR1/Script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const number0Films = [];
+    number0Films[0] = prompt("Сколько фильмов вы уже посмотрели", "");
+
+    Object.personalMovieDB = {
+        count:[0],
+        movies: {
+
+        movie:[number0Films[1]],
+
+        movie:[number0Films[2]]
+
+
+        },
+        actors:{
+
+        },
+        genres:[
+
+        ],
+        private:false
+
+    };
+
+    number0Films[1] = prompt("Один из последних просмотренных фильмов?", "");
+    number0Films[2] = prompt("На сколько оцените его?", "");
+
+    console.log(Object.personalMovieDB);
+
+
