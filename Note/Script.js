3a6f4c389d2669c5d6479fbfeb9211db52c8e909
Bogdan <thegarrybit@gmail.com>
2022-10-06 14:43:44 +0300

    Skills

--- Note/Script.js
+++ Note/Script.js
@@ -16,3 +16,22 @@ function getMathResult (a, b) {
 }
 }
 console.log(getMathResult(10, 3));
+
+
+
+const text = "wassup";
+console.log(text.toUpperCase()); //012345
+console.log(text.indexOf("s")); // wassup (s = 2)
+
+const slice = "yo nigga wassup";
+console.log(slice.slice(9, 16)); // slice - вырезать слово или букву и показать её
+console.log(slice.slice(9)); // можно указать только где начинается слово, он вырежит его до конца предл.
+console.log(slice.substring(9, 16)); // Вроде одно и тоже.  
+console.log(slice.substr(9, 6)); // вторая цифра означает кол-во символов а не номер строки. Функционал тот же.
+
+const num = 34.34;
+console.log(Math.round(num)); // Math.round - округляет число.
+
+const test = "12.2px";
+console.log(parseInt(test)); // переводит число в другую систему исчеслений.
+console.log(parseFloat(test)); // Выводит значение с плавающей точкой.
