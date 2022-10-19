e5ac51e39103159fc3c6224c1e5ad2a22caf1b02
Bogdan <thegarrybit@gmail.com>
2022-10-19 16:06:46 +0300

    note

--- Note/Script.js
+++ Note/Script.js
@@ -792,3 +792,12 @@
 //     return str.split(' ').join('');
 // }
 // console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+
+// function oddOrEven(array) {
+//     let sum = 0;
+//     array.forEach(element => {
+//         sum += element;
+//     });
+//     return sum % 2 ? 'odd' : 'even';
+// }
+// console.log(oddOrEven([11, 10]));
