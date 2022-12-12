6cc179591d281d123acadd1f2cc73842f6b3c645
Bogdan <thegarrybit@gmail.com>
2022-12-12 23:25:56 +0200

    codewars

--- Note/codewars.js
+++ Note/codewars.js
@@ -1998,23 +1998,30 @@
 // ]));
 
 function solution(list) {
-    if (list.length === 0) {return list;}
+    if (list.length === 0) {return '';}
     let res = '';
 
 
-    for (let i = 1; i < list.length; i++) {
-        if (list[i] - list[i - 1] === 1) {
-            continue;
-        } else {
-            res += `${list[i - 1]}-`;
-        }
+    // for (let i = 1; i < list.length; i++) {
+    //     if (list[i] - list[i - 1] === 1 && list[i + 1] - list[i] === 1) {
+    //         res += '-';
+    //         continue;
+    //     }
+        
+    //     res += `${list[i]},`;
+    // }
 
+    for (let i = 1; i < list.length; i++) {
         res += `${list[i]},`;
+        while(list[i] - list[i - 1] === 1) {
+            i++;
+            
+        }
     }
 
 
     return res;
 }
-console.log(solution([-6, -8, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));
+console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));
 
 // "-6,-3-1,3-5,7-11,14,15,17-20"   
