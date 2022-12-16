82ffd612663bbcb05ee56a027bb95e8894c3add0
Bogdan <thegarrybit@gmail.com>
2022-12-16 17:44:36 +0200

    codewars

--- Note/codewars.js
+++ Note/codewars.js
@@ -2061,7 +2061,7 @@
 
 // // "-6,-3-1,3-5,7-11,14,15,17-20"  
 
-function getGeneration(cells, generations){
+// function getGeneration(cells, generations){
     
-}
-console.log(getGeneration());
+// }
+// console.log(getGeneration());
