5ebac7e6d1e65f713f67f75646dd22b5c133d6da
Bogdan <thegarrybit@gmail.com>
2023-02-22 17:28:59 +0200

    leetcode

--- Note/leetcode.ts
+++ Note/leetcode.ts
@@ -201,31 +201,79 @@
 
 function checkInclusion(s1: string, s2: string): boolean {
     if (s1.split('').sort().join('') === s2.split('').sort().join('')) return true;
+
     const s1Len = s1.length;
     const s2Len = s2.length;
     const arr: number[] = [];
+    const letterIndexes: {[key: string]: number[]} = {};
+    let bestSum = 500;
+    let bestSumKey = '';
 
     for (let i = s2Len; i > 0; --i) {
         if (s2.substr(i, s1Len) === s1) return true
         if (s2.substr(i, s1Len) === s1.split('').reverse().join('')) return true
     }
 
-    for (let i = s1Len - 1; i >= 0; --i) {
-        if (s2.indexOf(s1[i]) !== -1) {
-            arr[arr.length] = s2.indexOf(s1[i]);
-            s2 = s2.replace(s1[i], ' ');
+    for (let i = 0; i < s2.length; ++i) {
+        if (!(s2.includes(s1[i])) && s1[i]) return false;
+        if (s1.includes(s2[i])) {
+            if (letterIndexes[s2[i]]) {
+                letterIndexes[s2[i]].push(i);
+            }
+            else {
+                letterIndexes[s2[i]] = [i];
+            }
         }
+    }
+
+    console.log(letterIndexes);
+
+    const copy = structuredClone(letterIndexes);
+
+    for (const [key, value] of Object.entries(letterIndexes)) {
+        if (value.length > 1 && value.length !== s1.match(new RegExp(key, 'g'))?.length) {
+            value.forEach(val => {
+                let i = 1;
+
+                while (Object.values(letterIndexes)[i]) {
+                    let j = 0;
+                    
+                    while (Object.values(letterIndexes)[i][j]) {
+                        const sum = val - Object.values(letterIndexes)[i][j];
+    
+                        if (sum < copy[key][j]) {
+                            copy[key] = [val];
+                            // console.log(copy[key]);
+                            // bestSumKey = key;
+                        }
+
+                        j++;
+                    }
+
+                    i++
+                }
+            });
+        }
+    }
+
+    if (bestSum !== 500) {
+        letterIndexes[bestSumKey] = [bestSum];
+    }
+
+    for (const value of Object.values(copy)) {
+        // console.log(...value);
+        arr.push(...value);
     }
 
     const indexes = Array.from(new Set(arr));
     const indexLen = indexes.length;
 
-    console.log(indexes);
-
     if (indexLen < s1Len) return false;
     if (indexLen === 1) return true;
-
+    
     indexes.sort((a, b) => a - b);
+
+    console.log(indexes);
 
     for (let i = 1; i < indexLen; ++i) {
         console.log(indexes[i - 1], indexes[i]);
@@ -234,7 +282,7 @@ function checkInclusion(s1: string, s2: string): boolean {
 
     return true;
 };
-console.log(checkInclusion('abc', 'bbbca'));
+console.log(checkInclusion('osmzg', 'diyhaywtgpzosgmuxvidndouo'));
 
 // function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
     
