8f50238d067da2b1e1416b7938f644db4721d130
Bogdan <thegarrybit@gmail.com>
2023-04-07 21:55:20 +0300

    codewars

--- Note/codewars.ts
+++ Note/codewars.ts
@@ -2541,13 +2541,10 @@
 // console.log(calculate(1)(1));
 
 Array.prototype.map = function (callback, thisArg) {
-    console.log(this);
     const newArr = [];
-
     for (let i = 0, arrLeng = this.length; i < arrLeng; ++i) {
-        const currElem = this[ i ];
-        currElem && newArr.push(callback.call(thisArg, currElem, i, this));
+        const currElem = this[i];
+        currElem !== undefined && newArr.push(callback.call(thisArg, currElem, i, this)) || newArr.push(undefined);
     }
-
     return newArr;
 };
