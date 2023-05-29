24df4904eb00ba54291ed6b3165e40b13aa5d3a9
Bogdan <thegarrybit@gmail.com>
2023-05-29 21:30:39 +0300

    codewars

--- Note/codewars.ts
+++ Note/codewars.ts
@@ -2666,8 +2666,82 @@
 // console.log(dog.name);
 // console.log(dog.name = 'Lessie');
 
-let newFunction = function () {
-    this.newFunction = newFunction;
-};
+// let newFunction = function () {
+//     this.newFunction = newFunction;
+// };
+//
+// console.log(new newFunction);
 
-console.log(new newFunction);
+class Animal {
+  name: string;
+  age: number;
+  legs: number;
+  species: string;
+  status: string;
+
+  constructor(
+      name: string,
+      age: number,
+      legs: number,
+      species: string,
+      status: string
+  ) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+
+  introduce() {
+    return `Hello, my name is ${ this.name } and I am ${ this.age } years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor(name: string, age: number, status: string) {
+    super(
+        name,
+        age,
+        0,
+        'shark',
+        status
+    );
+  }
+}
+
+class Cat extends Animal {
+  constructor(name: string, age: number, status: string) {
+    super(
+        name,
+        age,
+        4,
+        'cat',
+        status
+    );
+  }
+
+  introduce() {
+    return `Hello, my name is ${ this.name } and I am ${ this.age } years old.  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  master: string;
+
+  constructor(name: string, age: number, status: string, master: string) {
+    super(
+        name,
+        age,
+        4,
+        'dog',
+        status
+    );
+
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${ this.master }`;
+  }
+}
