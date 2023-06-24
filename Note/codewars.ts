d2afd02473399ba19e107bb8b544c0178a6bdb6f
Bogdan <thegarrybit@gmail.com>
2023-06-24 22:34:51 +0300

    codewars

--- Note/codewars.ts
+++ Note/codewars.ts
@@ -3283,16 +3283,26 @@
 // }
 
 
-function F(n) {
-  if (n > 0) {
-    return n - M(F(n - 1));
-  }
-  return 1;
-}
+// function F(n) {
+//   if (n > 0) {
+//     return n - M(F(n - 1));
+//   }
+//   return 1;
+// }
+//
+// function M(n) {
+//   if (n > 0) {
+//     return n - F(M(n - 1));
+//   }
+//   return 0;
+// }
 
-function M(n) {
-  if (n > 0) {
-    return n - F(M(n - 1));
-  }
-  return 0;
+function once(fn) {
+  let count = 0;
+  return (...args) => {
+    if (count === 0) {
+      count++;
+      return fn(...args);
+    }
+  };
 }
